01b6ba66a96631e77e0992d2c844d3c1cdb8214e
jerryk1925 <piplor40@gmail.com>
2018-03-22 00:31:20 +0300

    fixjs

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function () {
     $("#menu").on("click", "div", function (event) {
         event.preventDefault();
@@ -6,22 +5,16 @@ $(document).ready(function () {
             top = $(id).offset().top;
         $('body,html').animate({ scrollTop: top }, 1000);
     });
-});
-$(document).ready(function () {
     $('.header__top_nav').click(function () {
         $('.header__top_nav').toggleClass('clicked');
         $('.header__menu').css('display', 'flex');
         $('.header__top_nav').css('display', 'none');
-    })
-});
-$(document).ready(function () {
+    });
     $('.header__menu_close').click(function () {
         $('.header__menu_close').toggleClass('clicked');
         $('.header__menu').css('display', 'none');
         $('.header__top_nav').css('display', 'block');
-    })
-});
-$(document).ready(function () {
+    });
     $("#gotop").on("click", "a", function (event) {
         event.preventDefault();
         var id = $(this).attr('href'),
@@ -29,3 +22,5 @@ $(document).ready(function () {
         $('body,html').animate({ scrollTop: top }, 1000);
     });
 });
+
+
